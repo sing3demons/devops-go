77bf437873c48dae5f274579b555c1fce677fd67
sing3demons <sing3demons@live.com>
2022-11-26 17:25:29 +0700

    init project

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
+	"github.com/sing3demons/devopsgo/rest/handler"
+)
+
+func main() {
+	connStr := os.Getenv("DB_CONNECTION")
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	h := handler.NewApplication(db)
+
+	e := echo.New()
+	e.GET("/", h.Greeting)
+	// Intentionally, not setup database at this moment so we ignore feature to access database
+	// e.GET("/news", h.ListNews)
+	serverPort := ":" + os.Getenv("PORT")
+	e.Logger.Fatal(e.Start(serverPort))
+}
